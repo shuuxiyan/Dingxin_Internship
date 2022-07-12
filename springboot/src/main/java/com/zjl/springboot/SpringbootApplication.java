2d8306aa349819f3669a5ea46b07503e1763d072
shuuxiyan <1048257273@qq.com>
2022-07-12 11:36:24 +0800

    提交文件

---
+++ springboot/src/main/java/com/zjl/springboot/SpringbootApplication.java
@@ -0,0 +1,19 @@
+package com.zjl.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class SpringbootApplication {
+    @RequestMapping("/")
+    public static String index(){
+        return "hello spring boot";
+    }
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootApplication.class, args);
+    }
+
+}
